8286d078d8a6e288395bd5cd73090ec1aa2e677c
EbelAlfie <davis.onde.wei@gmail.com>
2024-09-06 22:26:23 +0700

    added component for login screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { useCreateChatClient } from 'stream-chat-react';
-import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
     
-    </BrowserRouter>
   );
 }
 
